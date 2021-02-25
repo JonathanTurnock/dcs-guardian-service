34a3ed9a53efb88d35fa8a8b2ceb7d4dba694160
Jonathan Turnock <Jonathan.Turnock@outlook.com>
2021-02-25 21:38:22 +0000

    Replace implementation of tasklist to not have deps Refactoring
    Update Docs

--- apps/dcs-server-guardian/src/main.ts
+++ apps/dcs-server-guardian/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['warn', 'error'],
+  });
+  const logger = new Logger('bootstrap');
+  const configService = app.get(ConfigService);
+
+  logger.log(`DCS Launching from ${configService.get('dcsServer.launcher')}`);
   await app.listen(45371);
 }
 
